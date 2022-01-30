724436d98d4e597fb6dba18227fa72886da72bd2
Jonathan Derque <jonathan.derque@gmail.com>
2022-01-30 16:46:39 +0100

    changed library name to lingword.wasm

--- build.zig
+++ build.zig
@@ -1,11 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    //const mode = b.standardReleaseOptions();
-
-    const lib = b.addSharedLibrary("lingword-wasm4", "src/main.zig", .unversioned);
+    const lib = b.addSharedLibrary("lingword", "src/main.zig", .unversioned);
     lib.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
     lib.setBuildMode(.ReleaseSmall);
 
